9e1e31c3af4b60e73b5a27328c99131545e13844
Jose Edmar De Siqueira <edmar.ade@gmail.com>
2025-11-27 15:45:08 -0300

    update vite.config

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,12 +2,27 @@ import { PlayCircleIcon } from 'lucide-react'
 import { Cycles } from '../Cycles'
 import { DefaultButton } from '../DefaultButton'
 import { DefaultInput } from '../DefaultInput'
+import React from 'react'
 
 export function MainForm() {
+  const [taskName, setTaskName] = React.useState('')
+
+  const handleCreateNewTask = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    console.log('Deu certo')
+  }
+
   return (
-    <form className="form">
+    <form className="form" onSubmit={handleCreateNewTask}>
       <div className="formRow">
-        <DefaultInput id="meuInput" type="text" labelText="Task" placeholder="Digite algo" />
+        <DefaultInput
+          id="meuInput"
+          type="text"
+          labelText="Task"
+          placeholder="Digite algo"
+          value={taskName}
+          onChange={(e) => setTaskName(e.target.value)}
+        />
       </div>
       <div className="formRow">
         <p>Nesse ciclo foque por 25 min.</p>
